14f727f9181b2480820f58c367f27903eaaacea6
roy-wang-py <wpyroy@qq.com>
2018-12-22 23:43:22 +0800

    Make the vehicle following the one in front, no crashing!

--- src/main.cpp
+++ src/main.cpp
@@ -256,12 +256,12 @@ int main() {
                 car_s = end_path_s;
             }
             bool too_close = false;
-#if 0
+
             //find ref_v to use
             for(int i = 0; i < sensor_fusion.size(); i++)
             {
                 //car is in my lane
-                float d = sensor_fsion[i][6];
+                float d = sensor_fusion[i][6];
                 if(d < (2+4*lane+2) && d>(2+4*lane-2))
                 {
                     double vx = sensor_fusion[i][3];
@@ -269,15 +269,15 @@ int main() {
                     double check_speed = sqrt(vx*vx + vy*vy);
                     double check_car_s = sensor_fusion[i][5];
 
-                    check_car_s += ((double)prev_size*0.2*check_speed);
+                    check_car_s += ((double)prev_size*0.02*check_speed);
 
                     if(check_car_s > car_s && (check_car_s - car_s)<30)
                     {
-                        ref_vel = 29.5;
+                        //ref_vel = 29.5;
+                        too_close = true;
                     }
                 }
             }
-
 
             if(too_close)
             {
@@ -285,9 +285,9 @@ int main() {
             }
             else if(ref_vel<49.5)
             {
-                ref_vel += 0.224
+                ref_vel += 0.224;
             }
-#endif
+
             vector<double> ptsx;
             vector<double> ptsy;
 
